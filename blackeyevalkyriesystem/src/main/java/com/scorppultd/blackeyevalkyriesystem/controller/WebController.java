09785dcef4848ada9112870a1c6afec37df0bc5a
Scorppu <eugenechan111@gmail.com>
2025-04-09 22:58:12 +0800

    added the autowired that was missing for some reason

--- blackeyevalkyriesystem/src/main/java/com/scorppultd/blackeyevalkyriesystem/controller/WebController.java
+++ blackeyevalkyriesystem/src/main/java/com/scorppultd/blackeyevalkyriesystem/controller/WebController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
